7770690e0a1f45c78e08af8815bd12d247f57402
Dhiraj Thapa <dhirajthapaparnami@gmail.com>
2024-06-11 13:55:50 +0545

    Added Stadium Model lol

--- draft/Scripts/ball.gd
+++ draft/Scripts/ball.gd
@@ -3,3 +3,8 @@ extends RigidBody3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+
+func _on_timer_timeout():
+	queue_free()
+	pass # Replace with function body.
