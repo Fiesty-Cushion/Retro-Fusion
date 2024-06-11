6834c91299c71533e1399fed19da1e8cccaa1f96
Dhiraj Thapa <dhirajthapaparnami@gmail.com>
2024-06-11 13:55:50 +0545

    Added Stadium Model lol

--- draft/Scripts/main.gd
+++ draft/Scripts/main.gd
@@ -3,26 +3,38 @@ extends Node3D
 @onready var mobile :CharacterBody3D = $bat;
 var connected_users : Dictionary ={}
 
-var BALL_VEL_MAX = 60
-var BALL_VEL_MIN = 45
+const BALL_VEL_MAX = 55
+const BALL_VEL_MIN = 45
 
-var quater : Quaternion = Quaternion(0, 0, 0, 1)
+var quater : Quaternion = Quaternion(0, 0, 0.7, 0.7)
+var REFERENCE_QUATER = Quaternion(0, 0, sqrt(2)/2, sqrt(2)/2)
 
 var ball_scene : PackedScene = preload("res://Scenes/ball.tscn")
 
+func _ready():
+	mobile.global_transform.basis = Basis(quater).scaled(global_transform.basis.get_scale())
+	print(_quater_relative(Quaternion(0, 0, 0.707, 0.707), Quaternion(0, 0, 0.707, 0.707)))
+
 func _init():
+	
 	Socket.start_web_socket();
 	# Signals
 	Globals.socket_data_recieved.connect(_on_socket_data_received)
 	Globals.user_connected.connect(_on_user_connected)
 	Globals.user_disconnected.connect(_on_user_disconnected)
 	
-
+func _quater_relative(x : Quaternion, y : Quaternion):
+	return x.inverse() * y
 	
 func _on_socket_data_received(json:JSON):
+	#print(json.data.ori)
+	var quater = Quaternion(json.data.ori.x, json.data.ori.y, json.data.ori.z, json.data.ori.w)
+	var axis : Vector3 = quater.get_axis()
+	
 	
 	var new_quater : Quaternion = Quaternion(-json.data.ori.x, -json.data.ori.z, json.data.ori.y , -json.data.ori.w )
-	quater = quater.slerp(new_quater, 0.8)
+	var relative_quater = _quater_relative(REFERENCE_QUATER, new_quater)	
+	quater = quater.slerp(relative_quater, 0.8)	
 	mobile.global_transform.basis = Basis(quater).scaled(global_transform.basis.get_scale())
 	
 	
@@ -53,7 +65,7 @@ func _on_user_disconnected(id):
 
 func spawn_ball():
 	var ball_pos: Vector3 = Vector3(0, 8.7, -50)
-	ball_pos.y = randf_range(8.5, 9.5)
+	ball_pos.y = randf_range(8.,9)
 	var ball := ball_scene.instantiate()
 	ball.position = ball_pos
 	ball.linear_velocity.z = randi_range(BALL_VEL_MIN, BALL_VEL_MAX)
