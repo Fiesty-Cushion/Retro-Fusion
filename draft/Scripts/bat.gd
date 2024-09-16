5571efa7de3336e224d3b3d94d91b50c8511c335
bishal0602 <bishalpanta60@gmail.com>
2024-09-16 22:08:20 +0545

    stadium scaled, floor physics friction+absorb, updated bat collision shape to reduce edge shots

--- draft/Scripts/bat.gd
+++ draft/Scripts/bat.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-const BAT_POSITION: Vector3 = Vector3(-2, 3.5, -7.0)
+const BAT_POSITION: Vector3 = Vector3(-1.25, 3.5, -6)
 
 var current_quaternion: Quaternion = Quaternion.IDENTITY
 var target_quaternion: Quaternion = Quaternion.IDENTITY
