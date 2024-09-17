8085a7b152d58552782474441eea0b99413ebc50
bishal0602 <bishalpanta60@gmail.com>
2024-09-18 00:35:54 +0545

    ball variations

--- draft/Scripts/ball.gd
+++ draft/Scripts/ball.gd
@@ -14,20 +14,51 @@ var camera : Camera3D
 var follow_ball = false
 var bat_hit = false
 
+enum BowlType { STRAIGHT, LEG_SPIN, OFF_SPIN, SEAM, SWING }
+var bowl_type : BowlType
+var wickets_position :Vector3
+const SPIN_FORCE := [8.0, 13.0, 20.0]
+const SWING_FORCE: float =20.0
+const SEAM_VARIATION: float = 20.0
+
+
 func _ready():
-	var ball_pos: Vector3 = Vector3(0, 8.7, -50)
-	ball_pos.y = randf_range(8.0, 9.0)
-	position = ball_pos
-
-	linear_velocity.z = randi_range(BALL_VEL_MIN, BALL_VEL_MAX)
-
 	contact_monitor = true
 	max_contacts_reported = 1
-	
 	camera = get_parent().get_node("Camera3D")
+	
+	bowl_type = BowlType.values()[randi() % BowlType.size()]
+	wickets_position = get_tree().get_first_node_in_group("wickets_marker").global_position
+	setup_bowl()
+
+func setup_bowl():
+	var ball_pos = Vector3(randf_range(-2, 2), randf_range(8.0, 9.0), -50)
+	position = ball_pos
+	
+	var direction = (wickets_position - position).normalized()
+	var speed = randf_range(BALL_VEL_MIN, BALL_VEL_MAX)
+	linear_velocity = direction * speed
+
+	match bowl_type:
+		BowlType.LEG_SPIN:
+			apply_torque(Vector3(0, 0, SPIN_FORCE[randi() % SPIN_FORCE.size()]))
+		BowlType.OFF_SPIN:
+			apply_torque(Vector3(0, 0, -SPIN_FORCE[randi() % SPIN_FORCE.size()]))
+		BowlType.SWING:
+			apply_central_force(Vector3(randf_range(-SWING_FORCE, SWING_FORCE),0,randf_range(-SWING_FORCE, SWING_FORCE)))
+		BowlType.SEAM:
+			# Apply spin after a delay
+			get_tree().create_timer(0.5).connect("timeout", Callable(self, "apply_seam"))
+			
+func apply_seam():
+	linear_velocity = linear_velocity * 1.3
+	apply_torque(Vector3(0,0,randf_range(-SEAM_VARIATION, SEAM_VARIATION)))	
 
 
 func _process(delta):
+	if (not bat_hit) && linear_velocity.length() < 30.0:
+		linear_velocity = linear_velocity.normalized() * 30.0
+		
 	if follow_ball:
 		# Define a direction vector (camera is behind the ball)
 		var direction: Vector3 = Vector3.BACK * 12.0
